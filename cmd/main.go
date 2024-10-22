f841d63cb5059d0ef13bf483d47fb45b9de57526
Sriteja R Chilakapati <sritejachilakapati@gmail.com>
2024-10-22 11:28:50 +0530

    feat: add mux router

--- cmd/main.go
+++ cmd/main.go
@@ -2,31 +2,81 @@ package main
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/sritejachilakapati/movietix/internal/config"
 	"github.com/sritejachilakapati/movietix/internal/database"
 	"github.com/sritejachilakapati/movietix/internal/repository"
 )
+
+func contentTypeApplicationJsonMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
+}
+
+type contextKey string
+
+const paramsKey contextKey = "params"
+
+func mapQueryParamsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		params := make(map[string][]string)
+		for key, value := range r.URL.Query() {
+			params[key] = value
+		}
+
+		ctx := context.WithValue(r.Context(), paramsKey, params)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+type jsonResponseWriter struct {
+	http.ResponseWriter
+}
+
+func (w jsonResponseWriter) WriteJson(statusCode int, data interface{}) (int, error) {
+	jsonBytes, err := json.Marshal(data)
+	if err != nil {
+		return 0, err
+	}
+
+	w.WriteHeader(statusCode)
+	return w.Write(jsonBytes)
+}
 
 func main() {
 	err := config.LoadEnv()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)
 	}
-	ctx := context.Background()
-	conn := database.Connect(ctx)
-	defer conn.Close(ctx)
 
-	userRepo := repository.New(conn)
+	r := mux.NewRouter()
+	r.Use(contentTypeApplicationJsonMiddleware, mapQueryParamsMiddleware)
+	r.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		jw := jsonResponseWriter{w}
 
-	users, err := userRepo.GetAllUsers(ctx)
-	if err != nil {
-		log.Fatalf("Error fetching users: %v", err)
-	}
+		ctx := r.Context()
+		conn := database.Connect(ctx)
+		defer conn.Close(ctx)
 
-	for _, user := range users {
-		log.Println(user)
-	}
+		params := r.Context().Value(paramsKey).(map[string][]string)
+		fmt.Printf("Params: %v\n", params)
 
+		userRepo := repository.New(conn)
+		users, err := userRepo.GetAllUsers(ctx)
+		if err != nil {
+			log.Fatalf("Error getting movies: %v", err)
+		}
+
+		jw.WriteJson(http.StatusOK, users)
+	}).Methods(http.MethodGet)
+
+	log.Println("Server started at :8080")
+	http.ListenAndServe(":8080", r)
 }
--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.21.0
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/pgx/v5 v5.6.0 // indirect
