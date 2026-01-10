6fc70d33b7b13d570858e7fec43c121e48bf92a0
Sriteja Chilakapati <sritejachilakapati@gmail.com>
2026-01-10 17:31:26 +0530

    Added GetMoviesByCity

---
+++ queries/movie.sql
@@ -0,0 +1,11 @@
+-- name: GetMoviesByCity :many
+SELECT DISTINCT
+    m.*
+FROM movies m
+         JOIN shows s ON s.movie_id = m.id
+         JOIN screens sc ON sc.id = s.screen_id
+         JOIN theaters t ON t.id = sc.theater_id
+WHERE t.city_code = $1
+  AND s.start_time > now()
+  AND s.status != 'cancelled'
+ORDER BY m.rating_score DESC NULLS LAST;
