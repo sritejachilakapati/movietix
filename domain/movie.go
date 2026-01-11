57be5f040f40c65dda3b27b3454e8b7fcb86fd43
Sriteja Chilakapati <sritejachilakapati@gmail.com>
2026-01-11 14:04:23 +0530

    Remove updated at and make runtime minutes non-nullable

--- domain/movie.go
+++ domain/movie.go
@@ -7,15 +7,14 @@ import (
 )
 
 type Movie struct {
-	ID             uuid.UUID  `json:"id"`
-	Title          string     `json:"title"`
-	Synopsis       string     `json:"synopsis"`
-	LanguageCode   string     `json:"languageCode"`
-	RuntimeMinutes int        `json:"runtimeMinutes"`
-	PosterURL      *string    `json:"posterUrl,omitempty"`
-	TrailerURL     *string    `json:"trailerUrl,omitempty"`
-	Rating         *float32   `json:"rating,omitempty"`
-	ReleaseDate    *time.Time `json:"releaseDate,omitempty"`
-	CreatedAt      time.Time  `json:"createdAt"`
-	UpdatedAt      time.Time  `json:"updatedAt"`
+	ID             uuid.UUID `json:"id"`
+	Title          string    `json:"title"`
+	Synopsis       string    `json:"synopsis"`
+	LanguageCode   string    `json:"languageCode"`
+	RuntimeMinutes int32     `json:"runtimeMinutes"`
+	PosterURL      *string   `json:"posterUrl,omitempty"`
+	TrailerURL     *string   `json:"trailerUrl,omitempty"`
+	Rating         *float32  `json:"rating,omitempty"`
+	ReleaseDate    time.Time `json:"releaseDate"`
+	CreatedAt      time.Time `json:"createdAt"`
 }
